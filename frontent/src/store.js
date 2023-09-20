10e436623da56cb1264b7d79b41fbcafc03aa674
Aban Muhammed <abancpchengani@gmail.com>
2023-09-20 13:42:12 +0530

    gistore post to header fixes

--- frontent/src/store.js
+++ frontent/src/store.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import { legacy_createStore as createStore } from "redux";
 import collections from "./configurations/collections";
 
-let initialState = {user: window.localStorage.getItem("token") ? axios.post(collections.server_base + "/get-user-details", { token: window.localStorage.getItem("token") ,headers: {"Access-Control-Allow-Origin": "*"}}).then((res) => { return res.data }) : null};
+let initialState = {user: window.localStorage.getItem("token") ? axios.post(collections.server_base + "/get-user-details", { token: window.localStorage.getItem("token") }).then((res) => { return res.data }) : null};
 
 const appReducer = (prevState = initialState, action) => {
-            return { user: window.localStorage.getItem("token") ? axios.post(collections.server_base + "/get-user-details", { token: window.localStorage.getItem("token"),headers: {"Access-Control-Allow-Origin": "*"} }).then((res) => {  return res.data }) : null };
+            return { user: window.localStorage.getItem("token") ? axios.post(collections.server_base + "/get-user-details", { token: window.localStorage.getItem("token") }).then((res) => {  return res.data }) : null };
         
     
 };
