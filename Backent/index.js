08e95cd0d8731cd2999c528e15e5821267cbaf04
Aban Muhammed <abancpchengani@gmail.com>
2023-09-20 13:35:50 +0530

    function key

--- Backent/index.js
+++ Backent/index.js
@@ -29,7 +29,7 @@ app.use(cors({
     origin: 'https://cartopedia-app.netlify.app'
   }));
 
-app.use((req,res,next)=>{
+app.use(function (req,res,next){
     res.setHeader('Access-Control-Allow-Origin', 'https://cartopedia-app.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
